6e95a358d4eee74a730e77afc8844e0bedcc2613
ddd-ctrl <jbr3488646034@163.com>
2025-07-21 10:26:33 +0800

    vscode的重新配置

---
+++ sum/sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main()
+{
+
+    int a=1;
+    int b=2;
+    std::cout<<"a+b="<<a+b<<std::endl;
+    std::cin>>a>>b;
+    int sum=a+b;
+    std::cout<<sum<<std::endl;
+    return 0;
+}
